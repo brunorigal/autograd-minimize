a87a28204e897274cd25f2d69bebeff1bf864f29
Bruno Rigal <bruno.rigal@odit-e.com>
2021-04-06 11:16:40 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='autograd_minimize',  
-    version='0.1.0',
+    version='0.1.1',
     author="Bruno Rigal",
     description="A wrapper of scipy minimize with automatic gradient and hessian computation.",
     long_description=long_description,
