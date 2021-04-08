e1f5c8457433bc09b008e25145e49c23319f2f09
Bruno Rigal <bruno.rigal@odit-e.com>
2021-04-08 14:25:15 +0200

    update tests

--- test/test_lib.py
+++ test/test_lib.py
@@ -9,6 +9,8 @@ from numpy.testing import assert_almost_equal
 from tensorflow import keras
 from tensorflow.keras import layers
 from autograd_minimize.tf_wrapper import tf_function_factory
+from autograd_minimize.torch_wrapper import torch_function_factory
+import torch.nn as nn
 
 
 def rosen_tst(backend='torch'):
@@ -240,9 +242,27 @@ def test_keras_model_regression():
                             layers.Dense(1)])
 
     # Transforms model into a function of its parameter
-    func = tf_function_factory(model, tf.keras.losses.MSE, X, y)
+    func, params = tf_function_factory(model, tf.keras.losses.MSE, X, y)
 
     # Minimization
-    res = minimize(func, [var.numpy() for var in model.trainable_variables], method='trust-constr')
-    
+    res = minimize(func, params, method='trust-constr')
+
+    assert_almost_equal(res.x[-1], -1, decimal=5)
+
+test_keras_model_regression()
+
+def test_torch_model_regression():
+    #### Prepares data
+    X = np.random.random((200, 2))
+    y = X[:,:1]*2+X[:,1:]*0.4-1
+
+    #### Creates model
+    model = nn.Sequential(nn.Linear(2, 1))
+
+    # Transforms model into a function of its parameter
+    func, params = torch_function_factory(model, nn.MSELoss(), X, y)
+
+    # Minimization
+    res = minimize(func, params, method='trust-constr', backend='torch')
+
     assert_almost_equal(res.x[-1], -1, decimal=5)
